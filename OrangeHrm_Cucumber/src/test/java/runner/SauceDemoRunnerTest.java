c675e0b4b6e4445c24e066572469a44d477e9619
jeevika <kirubajeevika@gmail.com>
2025-04-02 10:31:26 +0530

    third commit

--- OrangeHrm_Cucumber/src/test/java/runner/SauceDemoRunnerTest.java
+++ OrangeHrm_Cucumber/src/test/java/runner/SauceDemoRunnerTest.java
@@ -1,8 +1,17 @@
 package runner;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin={"pretty","html:target/cucumber-reports/Cucumber.html"},features="src\\test\\resources\\orange\\features\\SauceDemoLogin.feature",glue="stepDefinition")
-public class SauceDemoRunnerTest extends AbstractTestNGCucumberTests{
-	
+@CucumberOptions(
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/Cucumber.html",
+        "json:target/cucumber-reports/Cucumber.json"
+    },
+    features = "src\\test\\resources\\orange\\features\\SauceDemoLogin.feature",
+    glue = "stepDefinition"
+)
+public class SauceDemoRunnerTest extends AbstractTestNGCucumberTests {
+
 }
