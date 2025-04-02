1a52eae1921d9042afb66e3a372aaa9769e75388
jeevika <kirubajeevika@gmail.com>
2025-04-02 10:51:51 +0530

    forth commit

--- OrangeHrm_Cucumber/src/test/java/runner/SauceDemoRunnerTest.java
+++ OrangeHrm_Cucumber/src/test/java/runner/SauceDemoRunnerTest.java
@@ -6,7 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     plugin = {
         "pretty",
-        "html:target/cucumber-reports/Cucumber.html",
         "json:target/cucumber-reports/Cucumber.json"
     },
     features = "src\\test\\resources\\orange\\features\\SauceDemoLogin.feature",
